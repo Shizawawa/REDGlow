df9859e61074c5a010702f6f84217043ac278f62
rachel <benyoucef.rachel@gmail.com>
2018-06-13 14:06:00 +0200

    update

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,6 +12,13 @@ class DashboardController extends Controller
      */
     public function index()
     {
+    	$securityContext = $this->container->get('security.authorization_checker');
+		if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+			$token = $this->get('security.token_storage')->getToken();
+			$user = $token->getUser();
+		  	print_r($user);
+		}
+
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'Dashboard',
         ]);
