a0da60cf227df7bf816872367287129ee2e5029e
rachel <benyoucef.rachel@gmail.com>
2018-06-13 15:17:23 +0200

    home_page

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DashboardController extends Controller
 {
@@ -12,15 +14,22 @@ class DashboardController extends Controller
      */
     public function index()
     {
-    	$securityContext = $this->container->get('security.authorization_checker');
+    	/*$securityContext = $this->container->get('security.authorization_checker');
 		if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
 			$token = $this->get('security.token_storage')->getToken();
 			$user = $token->getUser();
 		  	print_r($user);
-		}
-
+		}*/
+		$securityContext = $this->container->get('security.authorization_checker');
+		if (false === $securityContext->isGranted('ROLE_USER')) {
+	        throw new AccessDeniedException('Unable to access this page!');
+	    }
+	    
+	    $token = $this->get('security.token_storage')->getToken();
+		$user = $token->getUser();
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'Dashboard',
+            'user' => $user,
         ]);
     }
 }
